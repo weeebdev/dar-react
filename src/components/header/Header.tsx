3e4a8cd3856a90da9af40f796af83faa0818362a
Adil <mr.adil777@gmail.com>
2021-01-13 10:57:19 +0600

    feat(header): add Header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,41 @@
 import React from 'react';
+import logo from '../../logo.svg';
+
 import './Header.scss';
 
 const Header: React.FC = () => {
-  return <div className="Header"></div>;
+  return (
+    <div className="Header">
+      <header>
+        <img src={logo} alt="Logo image" className="Header__logo" />
+        <nav className="Header__nav">
+          <ul className="Header__ul">
+            <li className="Header__li">
+              <a href="" className="Header__a">
+                О нас
+              </a>
+            </li>
+            <li className="Header__li">
+              <a href="" className="Header__a">
+                Обучение
+              </a>
+            </li>
+            <li className="Header__li">
+              <a href="" className="Header__a">
+                Сообщество
+              </a>
+            </li>
+            <li className="Header__li">
+              <a href="" className="Header__a">
+                Медиа
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <button className="Header__button">Войти</button>
+      </header>
+    </div>
+  );
 };
 
 export default Header;
