494cb384afa548ecd9e3a28776719d725c7dac6d
Adil <mr.adil777@gmail.com>
2021-01-13 10:59:46 +0600

    refactor(header): rename header component folder to Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
-import Header from './components/header/Header';
+import Header from './components/Header/Header';
 
 const App: React.FC = () => {
   return (
