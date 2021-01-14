6ec67530c9b48316f4c87b89b5ddf0cf4b7636a1
Adil <mr.adil777@gmail.com>
2021-01-14 17:46:18 +0600

    refactor: change scss to module.scss

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,38 +1,38 @@
 import React from 'react';
 import logo from '../../logo.svg';
 
-import './Header.scss';
+import styles from './Header.module.scss';
 
 const Header: React.FC = () => {
   return (
-    <div className="Header">
+    <div className={styles.Header}>
       <header>
-        <img src={logo} alt="logo" className="Header__logo" />
-        <nav className="Header__nav">
-          <ul className="Header__ul">
-            <li className="Header__li">
-              <a href="/" className="Header__a">
+        <img src={logo} alt="logo" className={styles.Header__logo} />
+        <nav className={styles.Header__nav}>
+          <ul className={styles.Header__ul}>
+            <li className={styles.Header__li}>
+              <a href="/" className={styles.Header__a}>
                 О нас
               </a>
             </li>
-            <li className="Header__li">
-              <a href="/" className="Header__a">
+            <li className={styles.Header__li}>
+              <a href="/" className={styles.Header__a}>
                 Обучение
               </a>
             </li>
-            <li className="Header__li">
-              <a href="/" className="Header__a">
+            <li className={styles.Header__li}>
+              <a href="/" className={styles.Header__a}>
                 Сообщество
               </a>
             </li>
-            <li className="Header__li">
-              <a href="/" className="Header__a">
+            <li className={styles.Header__li}>
+              <a href="/" className={styles.Header__a}>
                 Медиа
               </a>
             </li>
           </ul>
         </nav>
-        <button className="Header__button">Войти</button>
+        <button className={styles.Header__button}>Войти</button>
       </header>
     </div>
   );
--- src/components/MainNav/MainNav.tsx
+++ src/components/MainNav/MainNav.tsx
@@ -1,46 +1,46 @@
 import React from 'react';
 import logo from '../../logo.svg';
 
-import './MainNav.scss';
+import styles from './MainNav.module.scss';
 
 const MainNav = () => {
   return (
-    <div className="MainNav">
-      <nav className="MainNav__nav">
-        <ul className="MainNav__ul">
-          <li className="MainNav__li">
-            <a href="/" className="MainNav__a">
+    <div className={styles.MainNav}>
+      <nav className={styles.MainNav__nav}>
+        <ul className={styles.MainNav__ul}>
+          <li className={styles.MainNav__li}>
+            <a href="/" className={styles.MainNav__a}>
               Навыки
             </a>
           </li>
-          <li className="MainNav__li">
-            <a href="/" className="MainNav__a">
+          <li className={styles.MainNav__li}>
+            <a href="/" className={styles.MainNav__a}>
               Знания
             </a>
           </li>
-          <li className="MainNav__li">
-            <a href="/" className="MainNav__a">
+          <li className={styles.MainNav__li}>
+            <a href="/" className={styles.MainNav__a}>
               Карьера
             </a>
           </li>
-          <li className="MainNav__li">
-            <a href="/" className="MainNav__a">
+          <li className={styles.MainNav__li}>
+            <a href="/" className={styles.MainNav__a}>
               Технологии
             </a>
           </li>
-          <li className="MainNav__li">
-            <a href="/" className="MainNav__a">
+          <li className={styles.MainNav__li}>
+            <a href="/" className={styles.MainNav__a}>
               Идеи
             </a>
           </li>
-          <li className="MainNav__li">
-            <a href="/" className="MainNav__a">
+          <li className={styles.MainNav__li}>
+            <a href="/" className={styles.MainNav__a}>
               События
             </a>
           </li>
-          <li className="MainNav__li">
-            <a href="/" className="MainNav__a">
-              <img src={logo} alt="logo" className="MainNav__logo" />
+          <li className={styles.MainNav__li}>
+            <a href="/" className={styles.MainNav__a}>
+              <img src={logo} alt="logo" className={styles.MainNav__logo} />
             </a>
           </li>
         </ul>
